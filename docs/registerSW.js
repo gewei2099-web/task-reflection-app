317f3304e7b1d8b437d700693e63badda0678544
huanyu <huanyu.xu@tyjt-ai.com>
2026-03-03 11:54:50 +0800

    fix pwa start_url for home screen

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/task-reflection-app/sw.js', { scope: '/task-reflection-app/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
